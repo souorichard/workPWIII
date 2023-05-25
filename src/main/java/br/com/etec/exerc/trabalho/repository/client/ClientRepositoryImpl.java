f28dbe8770e102badce236714b0d1908171f61c6
souorichard <elaine.ra96@gmail.com>
2023-05-25 08:22:07 -0300

    Filtros Finalizados

--- src/main/java/br/com/etec/exerc/trabalho/repository/client/ClientRepositoryImpl.java
+++ src/main/java/br/com/etec/exerc/trabalho/repository/client/ClientRepositoryImpl.java
@@ -2,7 +2,9 @@ package br.com.etec.exerc.trabalho.repository.client;
 
 import br.com.etec.exerc.trabalho.model.Client;
 import br.com.etec.exerc.trabalho.repository.filter.ClientFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
@@ -12,6 +14,8 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientRepositoryImpl implements ClientRepositoryQuery {
 
@@ -29,12 +33,42 @@ public class ClientRepositoryImpl implements ClientRepositoryQuery {
         criteria.orderBy(builder.asc(root.get("name")));
 
         TypedQuery<Client> query = manager.createQuery(criteria);
+        addRestrictionsOfPagination(query, pageable);
 
-        return null;
+        return new PageImpl<>(query.getResultList(), pageable, total(clientFilter));
+    }
+
+    private Long total(ClientFilter clientFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Client> root = criteria.from(Client.class);
+
+        Predicate[] predicates = createRestrictions(clientFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("name")));
+
+        criteria.select(builder.count(root));
+
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private void addRestrictionsOfPagination(TypedQuery<Client> query, Pageable pageable) {
+        int currentPage = pageable.getPageNumber();
+        int totalRecordPerPage = pageable.getPageSize();
+        int primaryRegisterOfPage = currentPage * totalRecordPerPage;
+
+        query.setFirstResult(primaryRegisterOfPage);
+        query.setMaxResults(totalRecordPerPage);
     }
 
     private Predicate[] createRestrictions(ClientFilter clientFilter, CriteriaBuilder builder, Root<Client> root) {
-        return null;
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (!StringUtils.isEmpty(clientFilter.getName())) {
+            predicates.add(builder.like(builder.lower(root.get("name")), "%" + clientFilter.getName().toLowerCase() + "%"));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 
 }
--- src/main/java/br/com/etec/exerc/trabalho/repository/payaccount/PayAccountRepositoryImpl.java
+++ src/main/java/br/com/etec/exerc/trabalho/repository/payaccount/PayAccountRepositoryImpl.java
@@ -2,7 +2,9 @@ package br.com.etec.exerc.trabalho.repository.payaccount;
 
 import br.com.etec.exerc.trabalho.model.PayAccount;
 import br.com.etec.exerc.trabalho.repository.filter.PayAccountFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
@@ -12,6 +14,8 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PayAccountRepositoryImpl implements PayAccountRepositoryQuery {
 
@@ -29,12 +33,42 @@ public class PayAccountRepositoryImpl implements PayAccountRepositoryQuery {
         criteria.orderBy(builder.asc(root.get("date")));
 
         TypedQuery<PayAccount> query = manager.createQuery(criteria);
+        addRestrictionsOfPagination(query, pageable);
 
-        return null;
+        return new PageImpl<>(query.getResultList(), pageable, total(payAccountFilter));
+    }
+
+    private Long total(PayAccountFilter payAccountFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<PayAccount> root = criteria.from(PayAccount.class);
+
+        Predicate[] predicates = createRepositories(payAccountFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("date")));
+
+        criteria.select(builder.count(root));
+
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private void addRestrictionsOfPagination(TypedQuery<PayAccount> query, Pageable pageable) {
+        int currentPage = pageable.getPageNumber();
+        int totalRecordPerPage = pageable.getPageSize();
+        int primaryRegisterOfPage = currentPage * totalRecordPerPage;
+
+        query.setFirstResult(primaryRegisterOfPage);
+        query.setMaxResults(totalRecordPerPage);
     }
 
     private Predicate[] createRepositories(PayAccountFilter payAccountFilter, CriteriaBuilder builder, Root<PayAccount> root) {
-        return null;
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (!StringUtils.isEmpty(payAccountFilter.getDate())) {
+            predicates.add(builder.like(builder.lower(root.get("date")), "%" + payAccountFilter.getDate() + "%"));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 
 }
